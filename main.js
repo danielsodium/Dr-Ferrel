604705f0239473b6ff1b998646fdf9af0da507eb
Daniel <34946446+sodiumkid@users.noreply.github.com>
2019-09-24 22:27:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,12 @@ var author = ['Daniel Lee', 'Brian Lin', 'Daniel Lee', 'Richard Liu', 'Some kid 
 
 client.on('ready', () => {
   client.user.setActivity("students fail | .help", {type: "WATCHING"})
-  client.channels.get("626186938080034844").send("yeet dabs works");
+  client.channels.get("626186938080034844").send("Started");
+
+  con.connect(function(err) {
+    if (err) throw err;
+    client.channels.get("626186938080034844").send("Connected!");
+  });
   /*
   client.channels.get("622506890936713249").send({embed: {
     color: 58967,
@@ -165,12 +170,6 @@ function processCommand(message) {
   }
   if (primaryCommand == "role") {
     giveRole(message,splitCommand)
-  }
-  if (primaryCommand == "connect") {
-    con.connect(function(err) {
-      if (err) throw err;
-      message.channel.send("Connected!");
-    }
   }
 }
 
