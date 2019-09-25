c935342393236e9059732a28cb7e91ebfc3298c8
Daniel <34946446+sodiumkid@users.noreply.github.com>
2019-09-25 13:39:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -68,6 +68,10 @@ client.on("guildMemberAdd", (member) => {
   client.channels.get("619388377577422848").send("Hey " + member.toString() + ", welcome... we just do random stuff ap world idk");
 });
 
+client.on("guildMemberRemove", (member) => {
+  client.channels.get("619388377577422848").send("oh " + member.toString() + " left... well its better off without them....");
+});
+
 client.on('message', (message) => {
   if (message.author == client.user || message.author.bot) {
       return
